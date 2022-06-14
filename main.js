9da26dd08252982abe5a6b2e0e7ece8b5f337759
NB2010NB <natanbalai1@gmail.com>
2022-06-15 00:44:31 +0300

    new JS

---
+++ main.js
@@ -0,0 +1,55 @@
+// //*******1******** */
+
+// function sortArray(name) {
+//     name.sort();
+//     console.log(name);
+// }
+// sortArray(["natan", "avi", "dani", "lior", "israel", "dana"]);
+
+// //*********2******** */
+
+// function arrayNumber(number) {
+
+//     number.sort((a, b) => a - b);
+//     console.log(number);
+// } arrayNumber([4, 77, 89, 54, 2, 3, 66, 35])
+
+
+// //******3****** */
+// function numberArray(sortNumber) {
+//     sortNumber.sort((a, b) => b - a);
+//     console.log(sortNumber);
+// } numberArray([46, 23, 75, 8, 34, 35, 6]);
+
+//******4****** */
+// let arrayName = [];
+// let nameStudent = document.getElementById("name");
+// function nameClass() {
+//     if (arrayName.length < 10) {
+//         arrayName.push(nameStudent.value);
+//     }
+//     arrayName.sort()
+//     console.log(arrayName);
+// }
+
+//*******5***** */
+// let arrayNum = [];
+// function arrayRandom() {
+//     for (let i = 0; i < 10; i++) {
+//         let number = Math.floor(Math.random() * 60);
+//         arrayNum.push(number);
+//     }
+//     arrayNum.sort((a, b) => a - b);
+//     console.log(arrayNum);
+// } arrayRandom()
+
+//******6***** */
+// let nameArray = ["avi", "dana", "lea", "zigi", "yael", "aviel", "levi"];
+// nameArray.sort();
+// nameArray.join("--")
+// console.log(nameArray);
+
+//*****7****** */
+
+let arrayHiTac = ["google", "at&n", "trigo", "intel"];
+    console.log(arrayHiTac.value)
